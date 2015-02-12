e669bfad4380740a1db62f55b6915ef5f850925f
alykoff <galll-al@yandex.kz>
2015-02-12 14:44:36 +0300

    end check methods

--- app/controllers/OrderController.scala
+++ app/controllers/OrderController.scala
@@ -4,7 +4,7 @@ import models._
 import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc.{BodyParsers, Action, Controller}
-import controllers.SecureController.{Authenticated}
+import controllers.SecureController.Authenticated
 import play.api.libs.json.JsError
 import play.api.libs.json._
 import play.api.libs.json.util._
@@ -18,11 +18,6 @@ import scala.collection.immutable.TreeMap
 
 object OrderController extends Controller {
 
-  implicit def set2TreeSet[A](set: Set[A])(implicit ev1: Ordering[A]): TreeSet[A] = {
-    set.foldLeft(TreeSet.empty[A]) {case (acc, x) =>
-      acc + x
-    }
-  }
 
   implicit val positionReads: Reads[Position] = (
     (__ \ "id_item").read[Long] ~
@@ -52,43 +47,45 @@ object OrderController extends Controller {
 
   implicit val editOrderReads: Reads[EditOrder] = (
     (__ \ "id_order").read[Long] ~
-      (__ \ "edit_items").lazyRead(Reads.set[Position](positionReads))
-    ) (EditOrder.apply _)
+    (__ \ "edit_items").lazyRead(Reads.set[Position](positionReads))
+  ) (EditOrder.apply _)
   implicit val editOrderWrites: Writes[EditOrder] = (
     (__ \ "id_order").write[Long] ~
     (__ \ "edit_items").lazyWrite(Writes.set[Position](positionWrites))
   ) (unlift(EditOrder.unapply))
 
+  case class CreateOrder(items: Set[Position])
+  implicit val createOrderReads: Reads[CreateOrder] =
+    (__ \ "edit_items").lazyRead(Reads.set[Position](positionReads)).map(CreateOrder.apply)
+  implicit val createOrderWrites: Writes[CreateOrder] =
+    (__ \ "edit_items").lazyWrite(Writes.set[Position](positionWrites)).contramap(unlift(CreateOrder.unapply))
 
-//  implicit val orderReads: Reads[ProductType] = (
-//    (__ \ "id_item").read[Long]
-//  )(ProductType.apply _)
+  case class CheckOrder(idOrder: Long)
+  implicit val checkOrderReads: Reads[CheckOrder] = (__ \ "id_order").read[Long].map(CheckOrder.apply)
 
-  def create() = Authenticated { implicit request =>
-
-//    val re =
-//      (__ \ "id_order").write[Long] and
-//        (__ \ "id_user").write[Long] and
-//        (__ \ "positions").lazyWrite(Writes.seq[Position](positionWrites)
-////    re(unlift(Position.unapply))
-
-
-    Ok(request.user.email)
-  }
+//  def create() = Authenticated { implicit request =>
+//
+////    val re =
+////      (__ \ "id_order").write[Long] and
+////        (__ \ "id_user").write[Long] and
+////        (__ \ "positions").lazyWrite(Writes.seq[Position](positionWrites)
+//////    re(unlift(Position.unapply))
+//
+//
+//    Ok(request.user.email)
+//  }
 
   def msgSuccess(msg: String) = Json.obj("status" -> "ok", "message" -> msg)
 
   def edit(id: Long) = Authenticated(BodyParsers.parse.json) { implicit request =>
-    Logger.info("edit begin")
     val editOrderResult = request.body.validate[EditOrder]
-    Logger.info("edit begin2")
     editOrderResult.fold(
-      error => Ok("nozing1"),
+      error => Ok("Bad data input"),
       editOrder => {
         val user = request.user
         val rawOrder = Order.getById(editOrder.orderId).filter(_.idUser == user.id).filter(!_.status)
         rawOrder match {
-          case None => Ok("nothing2")
+          case None => Ok("order not found")
           case Some(order) =>
             Logger.info(s"editOrder: $editOrder")
             val newOrder = Order.edit(order, editOrder)
@@ -100,7 +97,6 @@ object OrderController extends Controller {
   }
 
   def get(id: Long) = Authenticated { implicit request =>
-    val re = """{"id_order": 1, "edit_items": [{"id_item": 1, "count": 99}, {"id_item": 2, "count": 99}, {"id_item": 3, "count": 99}]}"""
     Logger.info(Json.toJson(
       EditOrder(1L, Set(Position(1, 4), Position(2, 4), Position(3, 4), Position(6, 4)))
     ).toString)
@@ -115,8 +111,33 @@ object OrderController extends Controller {
     }
   }
 
-  def order = Action {implicit request =>
-    Ok(views.html.order())
+  def create = Authenticated(BodyParsers.parse.json) {implicit request =>
+    val editOrderResult = request.body.validate[CreateOrder]
+    editOrderResult.fold(
+      error => BadRequest("Bad data input"),
+      createOrder => {
+        val userId = request.user.id
+        val newOrder = Order.create(createOrder.items, userId)
+        Ok(msgSuccess("saved"))
+      }
+    )
+  }
+
+  def check(id: Long) = Authenticated { implicit request =>
+//    val checkOrderInput = request.body.validate[CheckOrder]
+//    checkOrderInput.fold(
+//      error => BadRequest("bad data"),
+//      checkOrder => {
+        Order.getById(id) match {
+          case Some(order) if order.idUser == request.user.id =>
+            Logger.info(s"order: ${order.positions}")
+            Order.check(id)
+            Ok(msgSuccess("checked"))
+          case Some(order) => BadRequest(UserController.msgErr("Permission error"))
+          case _ => BadRequest(UserController.msgErr("Not found order"))
+        }
+//      }
+//    )
   }
 
 }
--- app/models/Store.scala
+++ app/models/Store.scala
@@ -1,6 +1,7 @@
 package models
 
-import controllers.OrderController.EditOrder
+import controllers.OrderController.{CreateOrder, EditOrder}
+import play.api.Logger
 
 import scala.collection.immutable.TreeSet
 import scala.util.{Failure, Success, Try}
@@ -14,6 +15,10 @@ case class ProductType(idProduct: Long, name: String, cost: Double)
 case class Store(positions: TreeSet[Position])
 
 case class Order(idOrder: Long, idUser: Long, status: Boolean, positions: TreeSet[Position]) {
+  val BIGGER = 1
+  val SMALLER = -1
+  val EAQUALS = 0
+
   def getUpdatedOrder(newOrder: Set[Position]): Order = {
     newOrder match {
       case items if items.isEmpty => this
@@ -21,26 +26,26 @@ case class Order(idOrder: Long, idUser: Long, status: Boolean, positions: TreeSe
     }
   }
 
-  private def getUpdatedOrder(newPositions: TreeSet[Position]): Order = {
-    val BIGGER = 1
-    val SMALLER = -1
-    val EAQUALS = 0
-    def gt(value: Position)(other: Position) = value.compare(other) == 1
-    def accValue(acc: TreeSet[Position], updatedPositions: TreeSet[Position], value: Position) = {
-      val updatedMin = updatedPositions.min
-      value.compare(updatedMin) match {
-        case BIGGER =>
-          val newAcc = acc ++ updatedPositions.takeWhile(gt(value)) + value
-          val newUpdatedSet = updatedPositions.dropWhile(gt(value))
-          (newAcc, newUpdatedSet)
-        case SMALLER => (acc + value, updatedPositions)
-        case EAQUALS => {
-          val newAcc = acc + Position(value.idProduct, updatedMin.count)
-          val newUpdatedSet = updatedPositions.drop(1)
-          (newAcc, newUpdatedSet)
-        }
+  private def gt(value: Position)(other: Position) = value.compare(other) == BIGGER
+
+  private def accValue(acc: TreeSet[Position], updatedPositions: TreeSet[Position], value: Position) = {
+    val updatedMin = updatedPositions.min
+    value.compare(updatedMin) match {
+      case BIGGER =>
+        val newAcc = acc ++ updatedPositions.takeWhile(gt(value)) + value
+        val newUpdatedSet = updatedPositions.dropWhile(gt(value))
+        (newAcc, newUpdatedSet)
+      case SMALLER => (acc + value, updatedPositions)
+      case EAQUALS => {
+        val newAcc = acc + Position(value.idProduct, updatedMin.count)
+        val newUpdatedSet = updatedPositions.drop(1)
+        (newAcc, newUpdatedSet)
       }
     }
+  }
+
+  private def getUpdatedOrder(newPositions: TreeSet[Position]): Order = {
+
     def foldLeft = positions.foldLeft(TreeSet.empty[Position], newPositions) _
     val (updatedItems, newItems) = foldLeft { case ((acc, updatedPositions), value) =>
       updatedPositions match {
@@ -55,12 +60,62 @@ case class Order(idOrder: Long, idUser: Long, status: Boolean, positions: TreeSe
 }
 
 object Store {
+  val BIGGER = 1
+  val SMALLER = -1
+  val EAQUALS = 0
+
   var store = Store(TreeSet(Position(1, 12)) + Position(2, 22))
+
   def get(from: Long, to: Long) = Success(store)
   def getAll: Option[Store] = Some(store)
+
+  def checkOrder(items: TreeSet[Position]) = {
+    val result = store.positions.foldLeft((TreeSet.empty[Position], items)) {case ((acc, checkedItems), value) =>
+      checkedItems match {
+        case checkedItems: TreeSet[Position] if checkedItems.isEmpty => (acc + value, checkedItems)
+        case `checkedItems` => accValue(acc, checkedItems, value)
+      }
+    }
+    store = Store(result._1)
+    Logger.info(s"Store: ${store}")
+  }
+
+  private def gt(value: Position)(other: Position) = value.compare(other) == BIGGER
+
+  private def accValue(acc: TreeSet[Position], updatedPositions: TreeSet[Position], value: Position) = {
+    val updatedMin = updatedPositions.min
+    value.compare(updatedMin) match {
+      case BIGGER =>
+        val newAcc = acc ++ updatedPositions.takeWhile(gt(value)) + value
+        val newUpdatedSet = updatedPositions.dropWhile(gt(value))
+        (newAcc, newUpdatedSet)
+      case SMALLER => (acc + value, updatedPositions)
+      case EAQUALS => {
+        val newAcc = acc + Position(value.idProduct, value.count - updatedMin.count)
+        val newUpdatedSet = updatedPositions.drop(1)
+        (newAcc, newUpdatedSet)
+      }
+    }
+  }
 }
 
 object Order {
+  implicit def set2TreeSet[A](set: Set[A])(implicit ev1: Ordering[A]): TreeSet[A] = {
+    set.foldLeft(TreeSet.empty[A]) { case (acc, x) =>
+      acc + x
+    }
+  }
+
+  def nextId: Long = {
+    Option(orders.max(Ordering[Long].on[Order](_.idOrder))).map(_.idOrder + 1).getOrElse(1)
+  }
+  def create(items: Set[Position], userId: Long) = {
+    val order = Order(nextId, userId, false, items)
+    orders = order :: orders
+    Logger.info(order.toString)
+    order
+  }
+
   var orders = Order(1, 1, false, TreeSet(Position(1, 4), Position(2, 4), Position(3, 4), Position(6, 4))) ::
     Order(2, 1, false, TreeSet(Position(1, 4))) :: Order(3, 2, false, TreeSet(Position(1, 4))) :: List()
 
@@ -77,5 +132,18 @@ object Order {
     if (orders.filter(_.idOrder == newOrder.idOrder).nonEmpty)
       orders = (newOrder :: orders.takeWhile(_.idOrder != newOrder.idOrder)) ::: orders.dropWhile(_.idOrder == newOrder.idOrder)
     Some(newOrder)
+  }
+
+  def check(idOrder: Long): Boolean = {
+    orders.find(_.idOrder == idOrder) match {
+      case Some(order) =>
+        Store.checkOrder(order.positions)
+        orders.foldLeft(List.empty[Order]) {case (acc, x) =>
+          if (x.idOrder == idOrder) Order(x.idOrder, x.idUser, true, x.positions) :: acc
+          else acc
+        }
+        true
+      case _ => false
+    }
   }
 }
